32291e1146f883156ea6df12039f23cd0530de49
PXu114 <pxu1144@gmail.com>
2024-12-06 13:20:17 -0800

    Supposed organizations

---
+++ approximations.py
@@ -0,0 +1,5 @@
+"""
+Peter Xu, Guang Hua
+AMATH 531
+This file contains the plotting and approximations made on functions defined in equations.py.
+"""
---
+++ equations.py
@@ -0,0 +1,6 @@
+"""
+Peter Xu, Guang Hua
+AMATH 531
+This file contains the definition of differential equations from our final term paper.
+Details about the assumptions made by us can be found in the pdf file.
+"""
